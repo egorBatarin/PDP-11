2e8949a8d87aeb89bb4f61ff3b653ecd78fd51c7
egich <egorbatarin@yandex.ru>
2020-05-08 18:48:52 +0300

    add pdp-11_mm.c

---
+++ pdp11_mm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef unsigned char byte;
+typedef unsigned short int word;
+typedef word Adress;
+
+#define MEMSIZE (64*1024)
+
+byte mem[MEMSIZE];
+
+
+
+int main() {
+
+    return 0;
+}
