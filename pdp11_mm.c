99ca62f0ed55faa3d4db57c51b178de910827b67
egich <egorbatarin@yandex.ru>
2020-05-08 19:10:51 +0300

    add b_read b_write w_read + test

--- pdp11_mm.c
+++ pdp11_mm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 typedef unsigned char byte;
 typedef unsigned short int word;
@@ -8,9 +9,38 @@ typedef word Adress;
 
 byte mem[MEMSIZE];
 
+void b_write(Adress adr, byte b) {
+    mem[adr] = b;
+}
 
+byte b_read(Adress adr) {
+    return mem[adr];
+}
 
+word w_read(Adress adr) {
+    word w = ((word)mem[adr+1]) << 8;
+    w = w | mem[adr];
+    return w;
+}
+
+void test_mem() {
+    //пишем байт и читаем его
+    byte b0 = 0x0a;
+    b_write(3, b0); //пишем в 3 адрес массива mem символ b0
+    byte bres = b_read(3); //читаем то, что в 3 адресе (должен быть b0)
+    assert(b0 == bres);
+
+    //пишем 2 байта, читаем слово
+    Adress a = 5;
+    byte b1 = 0x0a;
+    byte b2 = 0x0c;
+    word w = 0x0b0a;
+    b_write(a, b1);
+    b_write(a + 1, b2);
+    word wres = w_read(a);
+    assert(wres == w);
+}
 int main() {
-
+    test_mem();
     return 0;
 }
