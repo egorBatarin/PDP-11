6e54eae674e8c102afcdb57ee660ffea3ac1c32f
egich <egorbatarin@yandex.ru>
2020-05-14 16:14:43 +0300

    обновил хедер для теста 01

--- foo.h
+++ foo.h
@@ -1,9 +1,13 @@
+#ifndef FOO_H
+
+#define FOO_H
 
 typedef unsigned char byte;
 typedef unsigned short int word;
 typedef word adr;
 
-#define MEMSIZE (64*1024)
+#define MEMSIZE (64*1024) //размер оперативной памяти
+#define pc reg[7] //то бишь последний регистр R7
 
 extern byte mem[MEMSIZE];
 
@@ -11,5 +15,23 @@ void b_write(adr a, byte b);
 byte b_read(adr a);
 word w_read(adr a);
 void w_write(adr a, word w);
+void run();
+void do_mov();
+void do_halt();
+void do_add();
+void do_nothing();
 
+
+extern const Command cmd[];
+void trace(const char *  format, ...);
+
+typedef struct {
+	word mask;
+	word opcode;
+	char * name;
+	void (*do_func)(void);
+} Command;
+
+
+#endif FOO_H
 
