c684e2a3b308d8ab471d0ba0af243fa975c1dc56
egich <egorbatarin@yandex.ru>
2020-05-14 16:31:04 +0300

    add run.c

---
+++ run.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h> // для функции exit
+#include "foo.h"
+
+void trace(const char *  format, ...) {
+
+
+};
+
+void run() {
+
+};
+
+void do_mov() {
+
+};
+
+void do_halt() {
+
+};
+
+void do_add() {
+
+};
+
+void do_nothing() {
+
+};
