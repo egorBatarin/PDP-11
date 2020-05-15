4ec40761baac759e6efd22b3dba6e0e9e436ed8c
egich <egorbatarin@yandex.ru>
2020-05-15 14:27:23 +0300

    добавил в хедер моды адресации, assert, а также структуру данных агрумент, читаемую с 6 битной последовательности

--- foo.h
+++ foo.h
@@ -2,13 +2,23 @@
 
 #define FOO_H
 #include <stdarg.h>
-
+#include <assert.h> // средство для сохранения зрения и времени
 typedef unsigned char byte;
 typedef unsigned short int word;
 typedef word adr;
 
 #define MEMSIZE (64*1024) //размер оперативной памяти
 #define pc reg[7] //то бишь последний регистр R7
+
+              // режимы адресации:
+#define Rn0 0 // регистровый
+#define Rn1 1 // косвенно-регистровый
+#define Rn2 2 // автоинкрементный
+#define Rn3 3 // косвенно автоинкрементный
+#define Rn4 4 // автодекрементный
+#define Rn5 5 // косвенно автодекрементный
+#define Rn6 6 // индексный
+#define Rn7 7 // косвенно индексный
 
 extern byte mem[MEMSIZE];
 
@@ -32,8 +42,12 @@ typedef struct {
 extern const Command cmd[];
 void trace(const char *  format, ...);
 
+struct Argument {
+    word val;
+    word adress;
+}; //объявим переменные ss и dd типа struct Argument
 
-
+extern struct Argument ss, dd;
 
 #endif FOO_H
 
