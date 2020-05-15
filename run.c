a527be92a7b2aad54edd5641ba9f3eec418486f3
egich <egorbatarin@yandex.ru>
2020-05-15 15:33:44 +0300

    Реализовал do_add и do_mov, сделал run

--- run.c
+++ run.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> // Г¤Г«Гї ГґГіГ­ГЄГ¶ГЁГЁ exit
+#include <stdlib.h>
 #include "foo.h"
 
 struct Argument ss, dd;
@@ -12,17 +12,17 @@ void trace(const char *  format, ...) {
 };
 
 void do_mov() {
-
+    w_write(dd.adress, ss.val); // просто пишем ss.val в слово с адресом dd.adress
 };
 
 void do_halt() {
 trace("R0 = %06o \n R1 = %06o \n R2 = %06o \n R3 = %06o \n R4 = %06o \n R5 = %06o \n R6 = %06o \n R7 = %06o \n" ,
-	reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]); //ГўГ»ГўГ®Г¤ГЁГ¬ ГўГ±ГҐ Г°ГҐГЈГЁГ±ГІГ°Г» ГЁ Г±ГІГ®ГЇ Г¬Г ГёГЁГ­Г 
+	reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]); // выводим все регистры
 	exit(0);
 };
 
 void do_add() {
-
+    w_write (dd.adress, ss.val + dd.val); // а тут просто складываем оба значения и туда же записываем
 };
 
 
@@ -35,7 +35,11 @@ void run() {
         int i;
         for(i = 0; i < 3; i++) {
             if ((w & cmd[i].mask) == cmd[i].opcode) {
-
+                if (i == 0| i == 1) { // если это mov или add, то
+                    dd = sixbittodata(w); //расшифровываем первые 6 бит (destination)
+                    ss = sixbittodata(w >> 6); // затем расшифровываем следующие 6 бит (source)
+                }
+                cmd[i].do_func(); // в любом случае выполняем функцию
             }
         }
     }
