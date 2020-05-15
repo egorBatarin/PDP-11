d9252d639a47d5d298a267f43839de79613041fe
egich <egorbatarin@yandex.ru>
2020-05-15 09:27:08 +0300

    добавил трассировочную печать, сделал в run проверку на совпадение последних 4 бит читаемых слов(пока без реализации), реализовал do_halt, которая показывает регистры и останавливает эмулятор

--- run.c
+++ run.c
@@ -3,12 +3,25 @@
 #include "foo.h"
 
 void trace(const char *  format, ...) {
-
-
+    va_list ap;
+	va_start(ap, format);
+	vprintf(format, ap);
+	va_end(ap);
 };
 
 void run() {
+    pc = 01000;
+    while(1) {
+        word w = w_read(pc);
+        trace("%06o %06o: ", pc, w);
+        pc += 2;
+        int i;
+        for(i = 0; i < 3; i++) {
+            if ((w & cmd[i].mask) == cmd[i].opcode ) {
 
+            }
+        }
+    }
 };
 
 void do_mov() {
@@ -16,7 +29,9 @@ void do_mov() {
 };
 
 void do_halt() {
-
+trace("R0 = %06o \n R1 = %06o \n R2 = %06o \n R3 = %06o \n R4 = %06o \n R5 = %06o \n R6 = %06o \n R7 = %06o \n" ,
+	reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]); //выводим все регистры и стоп машина
+	exit(0);
 };
 
 void do_add() {
